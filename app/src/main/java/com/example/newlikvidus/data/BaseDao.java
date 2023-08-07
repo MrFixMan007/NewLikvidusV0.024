bf3dc9358ee04860bd985fd7dd420518d01f9147
MrFixMan007 <schurkin.danil@yandex.ru>
2023-08-07 13:50:56 +0400

    added interface BaseDao

---
+++ app/src/main/java/com/example/newlikvidus/data/BaseDao.java
@@ -0,0 +1,22 @@
+package com.example.newlikvidus.data;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Update;
+
+public interface BaseDao<T> {
+    @Insert
+    long insert(T obj); //вернёт id вставленной записи
+    @Insert
+    long[] insertSome(T... obj); //вернёт несколько id вставленных записей
+
+    @Delete
+    void delete(T obj);
+    @Delete
+    int deleteSome(T... obj); //вернёт кол-во удалённых запиисей
+
+    @Update
+    void update(T obj);
+    @Update
+    int updateSome(T... obj); //вернёт кол-во обновлённых заисей
+}
